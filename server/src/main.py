9486ac90db84760136f0d5147681c07c64867dcb
solid0us <blt11797@gmail.com>
2025-09-15 20:20:31 -0700

    Added CORS

--- server/src/main.py
+++ server/src/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from models import init_db
 from routers import auth, flashcards, collections
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+CORS_ORIGIN=os.environ.get("CORS_ORIGIN")
+origins = CORS_ORIGIN.split(",")
 
 app = FastAPI()
+app.add_middleware(
+   CORSMiddleware,
+   allow_origins=origins,
+   allow_credentials=True,
+   allow_methods="*",
+   allow_headers="*"
+   
+)
 init_db()
 
 app.include_router(auth.auth_router)
