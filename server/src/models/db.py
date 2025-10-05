f01aa3f5547e9f5ba50fb02210b2527a3f5a5aee
solid0us <blt11797@gmail.com>
2025-10-05 15:54:17 -0700

    Added Testing env variable for mock in-memory db

--- server/src/models/db.py
+++ server/src/models/db.py
@@ -4,12 +4,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase
 load_dotenv()
 
-DB_USERNAME = os.environ.get("DB_USERNAME")
-DB_PASSWORD = os.environ.get("DB_PASSWORD")
-DB_HOST = os.environ.get("DB_HOST")
-DB_PORT = os.environ.get("DB_PORT")
-DB_NAME = os.environ.get("DB_NAME")
+TESTING = os.environ.get("TESTING", "false").lower() == "true"
 
-engine = create_engine(f"postgresql+psycopg2://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+if TESTING:
+   engine = create_engine("sqlite:///:memory:", connect_args={"check_same_thread": False})
+else:
+   DB_USERNAME = os.environ.get("DB_USERNAME")
+   DB_PASSWORD = os.environ.get("DB_PASSWORD")
+   DB_HOST = os.environ.get("DB_HOST")
+   DB_PORT = os.environ.get("DB_PORT")
+   DB_NAME = os.environ.get("DB_NAME")
+
+   engine = create_engine(f"postgresql+psycopg2://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
 class Base(DeclarativeBase):
    pass
